ee185535219fa8ee248d62dbcc484c324cda9954
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-12-08 06:31:12 -0600

    Set condition to the world controller actions.

--- app/controllers/world_controller.rb
+++ app/controllers/world_controller.rb
@@ -1,13 +1,17 @@
 class WorldController < ApplicationController
   def index
+  	@country = Country.order('name')
   end
 
   def by_population
+  	@country = Country.order('population DESC')
   end
 
   def by_age
+  	@country = Country.order('median_age').where('median_age > 30')
   end
 
   def by_required_service
+  	@country = Country.where(:mandatory_military_service => true).order('life_expectancy')
   end
 end
