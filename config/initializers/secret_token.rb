efa352eed151cf1dd9bd9c98d3db855fe4310848
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-12-07 19:40:59 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrepChallenge::Application.config.secret_token = '6c6bb907a3ebf758270cdd36d27651fb6bb606c1b984e32e1992d95bdddf7e4c8a3a4d59f8a3f3bb65a938c1931743c3baaa4fcb1e2bdf6a9d7476c2027c5007'
