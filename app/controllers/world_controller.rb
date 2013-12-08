829cb17f71a8f998de7bbb0f870821092c3072c9
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-12-08 09:31:39 -0600

    Add select dropdown to my search.

--- app/controllers/world_controller.rb
+++ app/controllers/world_controller.rb
@@ -16,11 +16,21 @@ class WorldController < ApplicationController
   end
 
   def search
+    @search_options = ['All Country', 'Country with mandatory military service', 'Countries without mandatory military service']
   end
 
   def search_results
     @keywords = params[:keyword]
-    @country = Country.where("name LIKE '%#{params[:keyword]}%'")
+    @search_selected = params[:mandatory_military_service]
+
+    if @search_selected == 'All Country'
+      @country = Country.where("name LIKE '%#{params[:keywords]}%'")
+    elsif @search_selected == 'Country with mandatory military service'
+      @country = Country.where("name LIKE '%#{params[:keywords]}%'").where(:mandatory_military_service => true)
+    elsif @search_selected == 'Countries without mandatory military service'
+      @country = Country.where("name LIKE '%#{params[:keywords]}%'").where(:mandatory_military_service => false)
+    end
+    
     if @country.empty?
       flash[:error] = "Country doesn't exist"
       redirect_to search_path
